643b0518de7d1b9c1a2fd16b536a9a3f95dd00cd
twinkle-sahoo34 <twinklekriti459@gmail.com>
2025-09-29 00:12:33 +0530

    ch

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+
   return (
-    <div className="flex h-screen items-center justify-center bg-black">
+    <div className="flex flex-col h-screen items-center justify-center bg-black gap-4">
       <Link href="/day0">
-        <button className="px-6 py-3 text-lg font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-600 transition">
+        <button
+          onClick={() => setMessage("Hello World")}
+          className="px-6 py-3 text-lg font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-600 transition"
+        >
           Day-0 &quot;Project Initialise&quot;
         </button>
       </Link>
+
+      
+      {message && <p className="text-white text-xl font-semibold">{message}</p>}
     </div>
   );
 }
+
