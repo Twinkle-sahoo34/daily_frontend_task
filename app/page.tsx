f2dc85debc2de132809aa5c868c160f78ab7778f
twinkle-sahoo34 <twinklekriti459@gmail.com>
2025-09-29 00:03:30 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex h-screen items-center justify-center bg-black">
       <Link href="/day0">
         <button className="px-6 py-3 text-lg font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-600 transition">
-          Day-0 "Project Initialise"
+          Day-0 &quot;Project Initialise&quot;
         </button>
       </Link>
     </div>
