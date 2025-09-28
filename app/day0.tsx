2947b46652dbc55cb276ee6c6afcd58e9ed8b625
twinkle-sahoo34 <twinklekriti459@gmail.com>
2025-09-28 23:21:29 +0530

    first commit

---
+++ app/day0.tsx
@@ -0,0 +1,7 @@
+export default function Day0() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-white">Hello World</h1>
+    </div>
+  );
+}
