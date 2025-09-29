94517c386a9eab1780e62883c93ae0674c13dcd7
twinkle-sahoo34 <twinklekriti459@gmail.com>
2025-09-29 22:21:08 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
         {/* Day-0 Button */}
         <Link href="/day0">
           <button className="w-64 px-6 py-3 text-lg font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-600 transition">
-            Day-0 "Project Initialise"
+            {"Day-0 \"Project Initialise\""}
           </button>
         </Link>
 
         {/* Day-1 Button */}
         <Link href="/day1">
           <button className="w-64 px-6 py-3 text-lg font-bold text-black bg-yellow-400 rounded-xl hover:bg-yellow-300 transition">
-            Day-1 🚀
+            {"Day-1 🚀"}
           </button>
         </Link>
       </div>
